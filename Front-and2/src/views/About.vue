7ea74df0f142995f10c3fbd0fb50c34f56ddcfb9
Jegirlana Santos <jegirlanasantossilva@gmail.com>
2020-10-10 13:21:48 -0300

    adicioando backAnd

---
+++ Front-and2/src/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>entrei aqui</h1>
+    
+    <Users></Users>
+    
+  </div>
+</template>
+
+<!--<script>
+ export default {
+ name: "About",
+ props: ["name","id"],
+ }
+</script>
+
+<!--<script>
+import Users from './views/Users.vue'
+
+
+export default {
+  name: "About",
+  components:{
+    Users,
+  },
+  methods: {
+    clique() {
+    this.$router.push({ name: "About", params: { name: "Maria",id:"15" }
+    }).catch(()=>{});
+    },
+ }
+}
+</script>-->
---
+++ Front-and2/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login">
+    <h1>Hi lorena</h1>
+    <h3>Operações com os clientes:</h3><br /><br />
+    <button @click="fetchUsers">Listar todos os clientes cadastrados</button><br /><br />
+    <button @click="postUser">Inserir clientes</button><br /><br />
+    
+    <p>{{ user }}</p><br /><br />
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <h4>{{ user.id }}</h4>
+        <p>{{ user.name }}</p>
+        <p>{{ user.email }}</p>
+        <p>{{ user.login }}</p>
+        <p>{{ user.telefone }}</p>
+        <p>{{ user.password }}</p>
+        <p>{{ user.confirmeSenha }}</p>
+        <p>{{ user.complemento }}</p>
+      </li>
+    </ul>
+  </div>
+  
+</template>
